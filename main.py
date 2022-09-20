6aa1f341206a716439ffad229213901ad8d25dc5
andrewjanedy <andy.janedy@gmail.com>
2022-09-20 13:02:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+student_list = ["Andrew", "Lillian", "Berta"]
+
+print(student_list)
+print(student_list[1])
+print(student_list[-1], student_list[-2])
+
+student_list.append("Craig")
+
+print(student_list)
+
+student_list[3] = "Thomas"
+
+print(student_list)
+
+student_list.insert(1, "Steve")
+
+print(student_list)
+
+student_list.sort()
+
+print(student_list)
