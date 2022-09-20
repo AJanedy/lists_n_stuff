a77778edb0064ba8c2231dc49e0a01c36fcd82c7
andrewjanedy <andy.janedy@gmail.com>
2022-09-20 13:18:59 -0400

    removed white space

--- main.py
+++ main.py
@@ -3,27 +3,15 @@ student_list = ["Andrew", "Lillian", "Berta"]
 print(student_list)
 print(student_list[1])
 print(student_list[-1], student_list[-2])
-
 student_list.append("Craig")
-
 print(student_list)
-
 student_list[3] = "Thomas"
-
 print(student_list)
-
 student_list.insert(1, "Steve")
-
 print(student_list)
-
 student_list.sort()
-
 print(student_list)
-
 student_list.pop(4)
-
 print(student_list)
-
 student_list.remove(student_list[0])
-
 print(student_list)
